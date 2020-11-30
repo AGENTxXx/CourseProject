c45c1d71879e44d685cabab3aa52077544b91064
Alexander Chuvashov <hackdiod@gmail.com>
2020-11-30 16:25:50 +0500

    Added fragments, changed activities names to fragments, add some resources for first fragment, changed activity_main.xml

---
+++ app/src/main/java/com/smog/courseproject/FragmentMoviesList.kt
@@ -0,0 +1,55 @@
+package com.smog.courseproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.card.MaterialCardView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentMoviesList.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+
+class FragmentMoviesList : Fragment() {
+    private var listener: CardFragmentClickListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cv = view.findViewById<MaterialCardView>(R.id.fragment_movie_list_cv_movie)
+
+        cv.setOnClickListener {
+            listener?.cardClick()
+        }
+    }
+
+
+
+    fun setClickListener(l: CardFragmentClickListener?) {
+        listener = l
+    }
+
+
+    interface CardFragmentClickListener {
+        fun cardClick()
+    }
+}
--- app/src/main/java/com/smog/courseproject/MainActivity.kt
+++ app/src/main/java/com/smog/courseproject/MainActivity.kt
@@ -3,9 +3,42 @@ package com.smog.courseproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),FragmentMoviesList.CardFragmentClickListener, FragmentMoviesDetails.OnBackPressedListener {
+
+    private lateinit var fragmentMoviesList:FragmentMoviesList
+    private lateinit var fragmentMoviesDetails:FragmentMoviesDetails
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.movie_details)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            fragmentMoviesList = FragmentMoviesList().apply { setClickListener(this@MainActivity) }
+            supportFragmentManager.beginTransaction()
+                .add(R.id.activity_main_fl, fragmentMoviesList,"first")
+                .commit()
+        }
+        else {
+            fragmentMoviesList = supportFragmentManager.findFragmentByTag("first") as FragmentMoviesList
+            fragmentMoviesList.setClickListener(this)
+
+            if (supportFragmentManager.findFragmentByTag("second") != null) {
+                fragmentMoviesDetails = supportFragmentManager.findFragmentByTag("second") as FragmentMoviesDetails
+                fragmentMoviesDetails.setClickListener(this)
+            }
+        }
     }
+
+    override fun cardClick() {
+        fragmentMoviesDetails = FragmentMoviesDetails().apply { setClickListener(this@MainActivity) }
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .add(R.id.activity_main_fl, fragmentMoviesDetails,"second")
+            .commit()
+    }
+
+    override fun doBack() {
+        onBackPressed()
+    }
+
+
 }
